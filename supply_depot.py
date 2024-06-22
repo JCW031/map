2fd9f69a4b7000a71507ac055cb22f8a51e762a4
JCW031 <aatt1015@gmail.com>
2024-06-22 23:06:45 +0900

    Update supply_depot.py

--- supply_depot.py
+++ supply_depot.py
@@ -1,5 +1,41 @@
 import streamlit as st
 from PIL import Image
+import requests
+
+# 회사 딕셔너리 생성
+companies = {
+    "㈜골드넥스": 1,
+    "㈜구하다": 2,
+    "㈜글로랑": 3,
+    "㈜넷앤드": 4,
+    "㈜다트미디어": 5,
+    "대원씨티에스㈜": 6,
+    "㈜더에스엠씨그룹": 7,
+    "델타일렉트로닉스코리아㈜": 8,
+    "레오버넷㈜": 9,
+    "㈜마쉬코리아보험중개": 10,
+    "메드트로닉코리아(유)": 11,
+    "㈜모비데이즈": 12,
+    "㈜알파브라더스": 13,
+    "앨리슨하이퍼앰㈜": 14,
+    "㈜에이치에너지": 15,
+    "엔시큐어㈜": 16,
+    "㈜엘리스그룹": 17,
+    "㈜엠비씨아트": 18,
+    "㈜엣지랭크": 19,
+    "오티스엘리베이터코리아(유)": 20,
+    "㈜오픈놀": 21,
+    "위즈코어㈜": 22,
+    "유한킴벌리㈜": 23,
+    "㈜이엠텍아이엔씨": 24,
+    "㈜중고나라": 25,
+    "㈜칸타코리아": 26,
+    "㈜티몬": 27,
+    "㈜포시에스": 28,
+    "㈜피알원": 29,
+    "㈜하이랜드푸드": 30,
+    "해줌": 31
+}
 
 # CSS 스타일을 사용하여 subheader 오른쪽 정렬 설정
 st.markdown(
@@ -28,48 +64,26 @@ st.sidebar.header('Log in')
 user_id = st.sidebar.text_input('아이디 입력', value='', max_chars=15)
 user_password = st.sidebar.text_input('패스워드 입력', value='', type='password')
 
-if user_id == 'pass' and user_password == '1234':
-    st.sidebar.header('Cj Portfolio')
+# GitHub에서 create_map 함수 가져오기
+url = "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/YOUR_REPOSITORY_NAME/main/create_map.py"
+response = requests.get(url)
+exec(response.text)
 
-    menu = st.sidebar.radio('회사 선택', [
-        "㈜골드넥스",
-        "㈜구하다",
-        "㈜글로랑",
-        "㈜넷앤드",
-        "㈜다트미디어",
-        "대원씨티에스㈜",
-        "㈜더에스엠씨그룹",
-        "델타일렉트로닉스코리아㈜",
-        "레오버넷㈜",
-        "㈜마쉬코리아보험중개",
-        "메드트로닉코리아(유)",
-        "㈜모비데이즈",
-        "㈜알파브라더스",
-        "앨리슨하이퍼앰㈜",
-        "㈜에이치에너지",
-        "엔시큐어㈜",
-        "㈜엘리스그룹",
-        "㈜엠비씨아트",
-        "㈜엣지랭크",
-        "오티스엘리베이터코리아(유)",
-        "㈜오픈놀",
-        "위즈코어㈜",
-        "유한킴벌리㈜",
-        "㈜이엠텍아이엔씨",
-        "㈜중고나라",
-        "㈜칸타코리아",
-        "㈜티몬",
-        "㈜포시에스",
-        "㈜피알원",
-        "㈜하이랜드푸드",
-        "해줌"
-    ])
+if user_id == 'pass' and user_password == '1234':
+    st.sidebar.header('가고 싶은 회사를 고르세요')
+
+    menu = st.sidebar.radio('회사 선택', list(companies.keys()))
 
     if menu:
-        st.sidebar.write(f'선택한 회사: {menu}')
-        # 여기에 선택한 회사에 따른 추가적인 작업을 수행할 수 있음
+        st.sidebar.write(f'선택한 회사: {menu}, 회사 번호: {companies[menu]}')
+
+        # 지도 생성 및 표시
+        depart = create_map()
+        depart.save('company_list.html')
+        st.markdown(
+            f'<iframe src="company_list.html" width="100%" height="800"></iframe>', 
+            unsafe_allow_html=True
+        )
+
     else:
         st.sidebar.warning('메뉴를 선택해주세요')
-
-
-    
